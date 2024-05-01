ef80e0761e316e24e679d31a0a7a4670dc32ef66
Ahmd970 <43095164+aahmd970@users.noreply.github.com>
2024-05-01 16:32:12 +0300

    Create Calculate Totals in Sales Order.js

---
+++ Calculate Totals in Sales Order.js
@@ -0,0 +1,45 @@
+frappe.ui.form.on('Sales Order', {
+    validate: function(frm) {
+        frm.trigger('calculate_totals');
+    },
+
+    calculate_totals: function(frm) {
+        var totalPanels = 0;
+        var totalTableTops = 0;
+        var totalDoors = 0;
+        var totalTableLegs = 0;
+        var totalPromotions = 0;
+        var totalPVC = 0;
+        
+    
+        // Iterate through each item
+        $.each(cur_frm.doc.items || [], function(i, item) {
+            var item_group = item.item_group;
+            var uom = item.uom;
+            var qty = flt(item.qty);
+    
+            // Check if the item belongs to the "Products" item group
+            if (item_group === "Panel") {
+                totalPanels += qty;
+            } else if (item_group === "Table Top") {
+                totalTableTops += qty;
+            } else if (item_group === "Door") {
+                totalDoors += qty;
+            } else if (item_group === "PVC") {
+                totalPVC += qty;
+            } else if (item_group === "Table Leg") {
+                totalTableLegs += qty;
+            } else if (item_group === "Promotion") {
+                totalPromotions += qty;
+            }
+        });
+    
+        // Update custom_nos_total and custom_meter_total fields with the total quantity
+        cur_frm.set_value('custom_total_panels', totalPanels);
+        cur_frm.set_value('custom_total_table_tops', totalTableTops);
+        cur_frm.set_value('custom_total_doors', totalDoors);
+        cur_frm.set_value('custom_total_pvc', totalPVC);
+        cur_frm.set_value('custom_total_legs', totalTableLegs);
+        cur_frm.set_value('custom_total_promotion', totalPromotions);
+    }
+});
